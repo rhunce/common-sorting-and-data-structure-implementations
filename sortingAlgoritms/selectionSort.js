082e3394f1a2be17ec6bf52d60553ff25062477b
Raymond Anthony Hunce <hunce18c@gmail.com>
2023-10-03 17:02:09 -0400

    Stub out and pseudocode selectionSort

---
+++ sortingAlgoritms/selectionSort.js
@@ -0,0 +1,11 @@
+// numbers = [5, 3, 1, 2, 4]
+
+// O(n^2) time | O(n) space
+// iterate over numbers from i = 0 to end - 1
+    // iterate over numbers from j = i to end - 1
+        // find min
+        // swap element at location of min with element at i
+// return numbers
+function selectionSort(numbers) {
+    // 
+}
