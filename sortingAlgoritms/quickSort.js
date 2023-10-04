1fd2db8b2d5e8ab40f799156f2a688308d797de3
Raymond Anthony Hunce <hunce18c@gmail.com>
2023-10-04 11:15:08 -0400

    Stub out quickSort implementation and test cases

---
+++ sortingAlgoritms/quickSort.js
@@ -0,0 +1,14 @@
+function quickSort(numbers) {
+    // TODO
+}
+
+// TEST CASES (ASSUMES ONLY NUMBERS IN ARRAY)
+console.log(insertionSort([]))
+console.log(insertionSort([-1.8]))
+console.log(insertionSort([2]))
+console.log(insertionSort([1, 5, 2]))
+console.log(insertionSort([-1, -2, -5]))
+console.log(insertionSort([5, 1, 2, 3, 4]))
+console.log(insertionSort([500, 400, 300, 200, 100]))
+console.log(insertionSort([-7, -2, 0, 1, 2, 3, 4, 5]))
+console.log(insertionSort([-1.8, 4, 2.55, 0, 5, 1, -2, 3]))
