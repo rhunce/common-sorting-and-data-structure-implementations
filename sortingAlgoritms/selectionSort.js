c89a3bdc7958c4a8050e65bc0555318fc539cbe9
Raymond Anthony Hunce <hunce18c@gmail.com>
2023-10-04 09:25:25 -0400

    Implement selectionSort

--- sortingAlgoritms/selectionSort.js
+++ sortingAlgoritms/selectionSort.js
@@ -1,11 +1,40 @@
-// numbers = [5, 3, 1, 2, 4]
-
-// O(n^2) time | O(n) space
-// iterate over numbers from i = 0 to end - 1
-    // iterate over numbers from j = i to end - 1
-        // find min
-        // swap element at location of min with element at i
-// return numbers
+// O(n^2) time | O(1) space
 function selectionSort(numbers) {
-    // 
+    for (let i = 0; i < numbers.length - 1; i++) {
+        const idxOfMinNumber = getIdxOfMinNumber(numbers, i)
+        if (i !== idxOfMinNumber) {
+            swap(numbers, i, idxOfMinNumber)
+        }
+    }
+    return numbers
 }
+
+// HELPERS
+function getIdxOfMinNumber(numbers, startIdx) {
+    let currentMinNum = Number.POSITIVE_INFINITY
+    let currentMinNumIdx = -1
+    for (let i = startIdx; i < numbers.length; i++) {
+        if (numbers[i] < currentMinNum) {
+            currentMinNum = numbers[i]
+            currentMinNumIdx = i
+        }
+    }
+    return currentMinNumIdx
+}
+
+function swap(numbers, idxOne, idxTwo) {
+    const temp = numbers[idxTwo]
+    numbers[idxTwo] = numbers[idxOne]
+    numbers[idxOne] = temp
+}
+
+// TEST CASES (ASSUMES ONLY NUMBERS IN ARRAY)
+console.log(selectionSort([]))
+console.log(selectionSort([-1.8]))
+console.log(selectionSort([2]))
+console.log(selectionSort([1, 5, 2]))
+console.log(selectionSort([-1, -2, -5]))
+console.log(selectionSort([5, 1, 2, 3, 4]))
+console.log(selectionSort([500, 400, 300, 200, 100]))
+console.log(selectionSort([-7, -2, 0, 1, 2, 3, 4, 5]))
+console.log(selectionSort([-1.8, 4, 2.55, 0, 5, 1, -2, 3]))
