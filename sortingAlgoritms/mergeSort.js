be9c304e5be6bde1e17d62be997691b6b6dcc203
Raymond Anthony Hunce <hunce18c@gmail.com>
2023-10-05 16:23:45 -0400

    Stub out mergeSort function

---
+++ sortingAlgoritms/mergeSort.js
@@ -0,0 +1,19 @@
+// O() time | O() space
+function mergeSort(numbers) {
+    // ...
+}
+
+// TEST CASES (ASSUMES ONLY NUMBERS IN ARRAY)
+console.log(quickSortHoare([]))
+console.log(quickSortHoare([-1.8]))
+console.log(quickSortHoare([2]))
+console.log(quickSortHoare([2, 1]))
+console.log(quickSortHoare([1, 5, 2]))
+console.log(quickSortHoare([-1, -2, -5]))
+console.log(quickSortHoare([3, 3, 3, 3, 3]))
+console.log(quickSortHoare([5, 4, 3, 2, 1]))
+console.log(quickSortHoare([-1, -2, -3, -4, -5]))
+console.log(quickSortHoare([5, 1, 2, 3, 4]))
+console.log(quickSortHoare([500, 400, 300, 200, 100]))
+console.log(quickSortHoare([-7, -2, 0, 1, 2, 3, 4, 5]))
+console.log(quickSortHoare([-1.8, 4, 2.55, 0, 5, 1, -2, 3]))
