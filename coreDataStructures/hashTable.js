358f780e7bf9a5c9d035ebaa5d2a2195363d1ba6
Raymond Anthony Hunce <hunce18c@gmail.com>
2023-10-11 13:35:20 -0400

    Implement HashTable set and get methods

--- coreDataStructures/hashTable.js
+++ coreDataStructures/hashTable.js
@@ -5,11 +5,23 @@ class HashTable {
     }
 
     set(key, value) {
-        // ...
+        const keyIdx = this._hash(key)
+        if (!this.table[keyIdx]) {
+            this.table[keyIdx] = []
+        }
+        const keyValuePairExists = this.table[keyIdx].find(valueAtKey => valueAtKey[0] === key)
+        if (!keyValuePairExists) {
+            this.table[keyIdx].push([key, value])
+            this.size++
+        }
     }
 
     get(key) {
-        // ...
+        const keyIdx = this._hash(key)
+        if (this.table[keyIdx]) {
+            return this._getValueForKey(key, this.table[keyIdx])
+        }
+        return null
     }
 
     remove() {
@@ -23,5 +35,23 @@ class HashTable {
         }
         return hash % this.table.length
     }
+
+    _getValueForKey(key, keyValues) {
+        return keyValues.find(keyValue => keyValue[0] === key)?.[1]
+    }
       
 }
+
+// TEST CASES
+console.log("========== TESTS FOR set AND get methods ==========")
+const hashTable = new HashTable()
+hashTable.set("A1", "A2")
+console.log("Should return A2: ", hashTable.get("A1"))
+hashTable.set("B1", "B2")
+console.log("Should return B2: ", hashTable.get("B1"))
+hashTable.set("C1", "C2")
+console.log("Should return C2: ", hashTable.get("C1"))
+console.log("Should return 3: ", hashTable.size)
+hashTable.set("C1", "C2")
+console.log("Should return C2: ", hashTable.get("C1"))
+console.log("Should return 3: ", hashTable.size)
