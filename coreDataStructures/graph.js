8d768adca6bc4f0e5857b7d0a562d63937a090a7
Raymond Anthony Hunce <hunce18c@gmail.com>
2023-10-11 19:19:00 -0400

    Implement addEdge and addNode methods

--- coreDataStructures/graph.js
+++ coreDataStructures/graph.js
@@ -26,3 +26,44 @@ class Node {
         return this.adjacents.indexOf(node) !== -1
     }
 }
+
+// Adjacency list graph. Object oriented implementation.
+class Graph {
+    constructor(edgeDirection = Graph.DIRECTED) {
+        this.nodes = new Map();
+        this.edgeDirection = edgeDirection;
+    }
+
+    addNode(node) {
+        const nodeInGraph = !!this.nodes.get(node.value)
+        if (!nodeInGraph) {
+            this.nodes.set(node.value, node)
+        }
+    }
+
+    addEdge(source, destination) {
+        this.addNode(source)
+        this.addNode(destination)
+        source.addAdjacent(destination)
+        if (this.edgeDirection === Graph.UNDIRECTED) {
+            destination.addAdjacent(source)
+        }
+        return [source, destination]
+    }
+
+
+}
+
+Graph.UNDIRECTED = Symbol('undirected graph'); // two-ways edges
+Graph.DIRECTED = Symbol('directed graph'); // one-way edges
+
+
+// const nodeAU = new Node("aU")
+// const nodeBU = new Node("bU")
+// const undirectedGraph = new Graph(Graph.UNDIRECTED)
+// const [a, b] = undirectedGraph.addEdge(nodeAU, nodeBU)
+
+// const nodeAD = new Node("aD")
+// const nodeBD = new Node("bD")
+// const directedGraph = new Graph(Graph.DIRECTED)
+// const [c, d] = directedGraph.addEdge(nodeAD, nodeBD)
