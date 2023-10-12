4a7d61be7a897962e7aff6020d2326956d96dec2
Raymond Anthony Hunce <hunce18c@gmail.com>
2023-10-11 20:52:55 -0400

    Add tests

--- coreDataStructures/graph.js
+++ coreDataStructures/graph.js
@@ -27,7 +27,7 @@ class Node {
     }
 }
 
-// Adjacency list graph. Object oriented implementation.
+// Adjacency list graph. Hash Map implementation.
 class Graph {
     constructor(edgeDirection = Graph.DIRECTED) {
         this.nodes = new Map();
@@ -35,7 +35,7 @@ class Graph {
     }
 
     addNode(value) {
-        const nodeInGraph = !!this.nodes.get(value)
+        const nodeInGraph = this.nodes.get(value)
         if (!nodeInGraph) {
             const newNode = new Node(value)
             this.nodes.set(value, newNode)
@@ -81,3 +81,34 @@ class Graph {
 
 Graph.UNDIRECTED = Symbol('undirected graph'); // two-ways edges
 Graph.DIRECTED = Symbol('directed graph'); // one-way edges
+
+console.log("========== TESTS FOR Directed Graph ==========")
+const directedGraph = new Graph(Graph.DIRECTED)
+directedGraph.addNode("a")
+directedGraph.addNode("b")
+directedGraph.addNode("c")
+console.log("Should return nodes a, b, and c in Map: ", directedGraph.nodes)
+directedGraph.addEdge("a", "b")
+console.log("Node 'a' should have adjacency to 'b': ", directedGraph.nodes)
+directedGraph.addEdge("a", "c")
+console.log("Node 'a' should have adjacency to 'c': ", directedGraph.nodes)
+directedGraph.removeNode("c")
+console.log("Node 'a' should have one adjacency to 'b'. 'c' should be gone: ", directedGraph.nodes)
+directedGraph.removeEdge("a", "b")
+console.log("Node 'a' should have no adjacencies: ", directedGraph.nodes)
+
+
+console.log("========== TESTS FOR Undirected Graph ==========")
+const undirectedGraph = new Graph(Graph.UNDIRECTED)
+undirectedGraph.addNode("x")
+undirectedGraph.addNode("y")
+undirectedGraph.addNode("z")
+console.log("Should return nodes x, y, and z in Map: ", undirectedGraph.nodes)
+undirectedGraph.addEdge("x", "y")
+console.log("Nodes 'x' and 'y' should have adjacencies to each other: ", undirectedGraph.nodes)
+undirectedGraph.addEdge("x", "z")
+console.log("Nodes 'x' and 'z' should have adjacencies to each other: ", undirectedGraph.nodes)
+undirectedGraph.removeNode("z")
+console.log("Node 'x' should have one adjacency to 'y'. 'z' should be gone: ", undirectedGraph.nodes)
+undirectedGraph.removeEdge("x", "y")
+console.log("Nodes 'x' and 'y' should have no adjacencies: ", undirectedGraph.nodes)
