e2f375353d5b658f4da28d3041b6fe412d33d5a6
Raymond Anthony Hunce <hunce18c@gmail.com>
2023-10-17 17:05:01 -0400

    Implement insertAtHead, insertAtTail, insertAtPosition methods and helpers

--- coreDataStructures/DoublyLinkedList.js
+++ coreDataStructures/DoublyLinkedList.js
@@ -19,15 +19,78 @@ class DoublyLinkedList {
     }
 
     insertAtHead(value) {
-        // ...
+        const node = new Node(value)
+        this.insertAtPosition(node, 1)
+        return node
     }
 
     insertAtTail(value) {
-        // ...
+        const node = new Node(value)
+        this.insertAtPosition(node, this.size + 1)
+        return node
     }
 
+
     insertAtPosition(value, position) {
-        // ...
+        if (position - this.size > 0) return null
+        const nodeToInsert = new Node(value)
+
+        // Empty Doubly Linked List
+        if (!this.head) {
+            this.head = nodeToInsert
+            this.tail = nodeToInsert
+            this.size++
+            return nodeToInsert   
+        }
+
+        // Inserting at head
+        if (position === 1) {
+            this._spliceNewNodeInAtTargetNode(nodeToInsert, this.head)
+            return nodeToInsert
+        }
+
+        // Inserting at tail
+        if (position === this.size + 1) {
+            this._spliceNewNodeInAtTargetNode(nodeToInsert)
+            return nodeToInsert
+        }
+
+        let currentNode = this.head
+        let currentPosition = 1
+        while (currentPosition !== position) {
+            currentNode = currentNode.next
+            currentPosition++
+        }
+        this._spliceNewNodeInAtTargetNode(nodeToInsert, currentNode)
+        return nodeToInsert
+    }
+
+    _spliceNewNodeInAtTargetNode(nodeToInsert, targetNode = null) {
+        // Inserting new head
+        if (targetNode === this.head) {
+            targetNode.prev = nodeToInsert
+            nodeToInsert.next = targetNode
+            this.head = nodeToInsert
+            this.size++
+            return
+        }
+
+        // Inserting new tail 
+        if (!targetNode) {
+            this.tail.next = nodeToInsert
+            nodeToInsert.prev = this.tail
+            this.tail = nodeToInsert
+            this.size++
+            return
+        }
+
+        // Inserting in the middle somewhere
+        targetNode.prev.next = nodeToInsert
+        nodeToInsert.prev = targetNode.prev
+        targetNode.prev = nodeToInsert
+        nodeToInsert.next = targetNode
+        this.size++
+        return 
     }
 
     updateHead(newValue) {
@@ -54,9 +117,22 @@ class DoublyLinkedList {
         // ...
     }
 
-    // Tunnels from head to tail and pushes values into array and returns array
     getDoublyLinkedListValues() {
-        // ...
+        if (!this.head) return null
+        const values = []
+        let currentNode = this.head
+        while (currentNode) {
+            values.push(currentNode.value)
+            currentNode = currentNode.next
+        }
+        return values
     }
 
 }
+
+// TEST CASES
+const dLL = new DoublyLinkedList()
+console.log("========== TESTS FOR insertAtHead ==========")
+// ...start inserting at head and testing using getDoublyLinkedListValues method
+console.log("========== TESTS FOR insertAtTail ==========")
+console.log("========== TESTS FOR insertAtPosition ==========")
