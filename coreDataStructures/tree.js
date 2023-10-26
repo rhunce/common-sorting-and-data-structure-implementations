3c88f2ee8f16c08426a9b6d524125f630c8b3a46
Raymond Anthony Hunce <hunce18c@gmail.com>
2023-10-26 17:06:49 -0400

    Refactor the Tree class

--- coreDataStructures/tree.js
+++ coreDataStructures/tree.js
@@ -1,25 +1,27 @@
+const Queue = require("./Queue")
+
 class Node {
     constructor(value) {
         this.value = value
         this.children = []
     }
-}
-
-class Tree {
-    constructor(node) {
-        this.root = node
-    }
 
     // O(1) time | O(1) space
-    insertNode(parentNode, childNode) {
-        parentNode.children.push(childNode)
+    addChild(...nodes) {
+        this.children.push(...nodes)
     }
 
     // O(c) time | O(1) space
-    // c = # of children in parent node
-    removeNode(parentNode, childNode) {
-        const childNodeIndex = parentNode.children.indexOf(childNode)
-        return childNodeIndex > -1 ? parentNode.children.splice(childNodeIndex, 1) : null
+    // c = # of children for node
+    removeChild(node) {
+        const childNodeIndex = this.children.indexOf(node)
+        return childNodeIndex > -1 ? this.children.splice(childNodeIndex, 1) : null
+    }
+}
+
+class Tree {
+    constructor(root) {
+        this.root = root
     }
 
     // O(n + e) time | O(d) space
@@ -36,16 +38,18 @@ class Tree {
     }
 
 
-    // If implemented with an actual Queue
     // O(n + e) time | O(n) space
     // n = # of nodes in tree
     // e = # of edges in tree
     breadthFirstSearch(value, node = this.root) {
-        let queue = [node]
-        while (queue.length) {
-            const currentNode = queue.shift()
+        const queue = new Queue()
+        queue.enqueue(node)
+        while (!queue.isEmpty()) {
+            const currentNode = queue.dequeue()
             if (currentNode.value === value) return currentNode
-            queue.push(...currentNode.children)
+            for (const child of currentNode.children) {
+                queue.enqueue(child)
+            }
         }
         return null
     }
@@ -82,20 +86,17 @@ function runTreeTests() {
     const node5 = new Node(5)
     const node6 = new Node(6)
     const node7 = new Node(7)
-    tree1.insertNode(rootNode, node2)
-    tree1.insertNode(rootNode, node3)
-    tree1.insertNode(rootNode, node4)
-    tree1.insertNode(node3, node5)
-    tree1.insertNode(node4, node6)
-    tree1.insertNode(node4, node7)
+    rootNode.addChild(node2, node3, node4)
+    node3.addChild(node5)
+    node4.addChild(node6, node7)
     console.log("Test 2: ", JSON.stringify(tree1.root) === JSON.stringify({value:1,children:[{value:2,children:[]},{value:3,children:[{value:5,children:[]}]},{value:4,children:[{value:6,children:[]},{value:7,children:[]}]}]}) ? "PASS" : "FAIL")
     console.log("Test 3: ", JSON.stringify(node2) === JSON.stringify({value:2,children:[]}) ? "PASS" : "FAIL")
     console.log("Test 4: ", JSON.stringify(node3) === JSON.stringify({value:3,children:[{value:5,children:[]}]}) ? "PASS" : "FAIL")
     console.log("Test 5: ", JSON.stringify(node4) === JSON.stringify({value:4,children:[{value:6,children:[]},{value:7,children:[]}]}) ? "PASS" : "FAIL")
     console.log("========== TESTS FOR removeNode ==========")
-    tree1.removeNode(node4, node7)
+    node4.removeChild(node7)
     console.log("Test 6: ", JSON.stringify(node4) === JSON.stringify({value:4,children:[{value:6,children:[]}]}) ? "PASS" : "FAIL")
-    tree1.removeNode(rootNode, node3)
+    rootNode.removeChild(node3)
     console.log("Test 7: ", JSON.stringify(tree1.root) === JSON.stringify({value:1,children:[{value:2,children:[]},{value:4,children:[{value:6,children:[]}]}]}) ? "PASS" : "FAIL")
     console.log("========== TESTS FOR depthFirstSearch ==========")
     console.log("Test 8: ", JSON.stringify(tree1.depthFirstSearch(1)) === JSON.stringify({value:1,children:[{value:2,children:[]},{value:4,children:[{value:6,children:[]}]}]}) ? "PASS" : "FAIL")
@@ -110,11 +111,11 @@ function runTreeTests() {
     console.log("Test 17: ", JSON.stringify(tree1.breadthFirstSearch(7)) === "null" ? "PASS" : "FAIL")
     console.log("========== TESTS FOR getDepth ==========")
     console.log("Test 18: ", tree1.getDepth() === 2 ? "PASS" : "FAIL")
-    tree1.removeNode(node4, node6)
+    node4.removeChild(node6)
     console.log("Test 19: ", tree1.getDepth() === 1 ? "PASS" : "FAIL")
-    tree1.removeNode(rootNode, node4)
+    rootNode.removeChild(node4)
     console.log("Test 20: ", tree1.getDepth() === 1 ? "PASS" : "FAIL")
-    tree1.removeNode(rootNode, node2)
+    rootNode.removeChild(node2)
     console.log("Test 21: ", tree1.getDepth() === 0 ? "PASS" : "FAIL")
 }
 // runTreeTests()
